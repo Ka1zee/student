e5baae4a59c6e1d6faef110c228c4ffa36fea56d
Ka1zee <Krutjiyt@gmail.com>
2025-03-18 00:20:16 +0200

    add

---
+++ Qrcode/qrcode/qrcode/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using QRCoder;
+
+namespace qrcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Силка для генерації QR-кода:");
+            string url = Console.ReadLine();
+
+            // Генерация QR-кода
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            // Сохранение изображения в папке
+            string folderPath = @"C:\QRCodeImages";
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            string filePath = Path.Combine(folderPath, "qrcode.png");
+            qrCodeImage.Save(filePath);
+
+            Console.WriteLine($"QR-код збережений: {filePath}");
+        }
+    }
+}
